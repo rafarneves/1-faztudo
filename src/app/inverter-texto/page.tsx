a62711807a4ed5de6979beec1d4bd5ec61111531
Rafael Neves <rafael.neves@ufrn.br>
2024-09-22 21:20:10 -0300

    fix: ajuste

--- src/app/inverter-texto/page.tsx
+++ src/app/inverter-texto/page.tsx
@@ -6,7 +6,6 @@ export default function inverterTexto() {
     const inverterString = () => {
         const textoEntrada = document.getElementById('input-text')
         const textoSaida = document.getElementById('output-text')
-
         textoSaida?.value = textoEntrada?.value.split('').reverse().join('');
     }
     
